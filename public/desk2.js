51ec31d2a0cb4aaf2bded8165f5de77edfe9fd89
Preisler <preisler.andras@gmail.com>
2023-06-20 22:59:14 +0200

    creating desks

---
+++ public/desk2.js
@@ -0,0 +1,30 @@
+const socket = io('http://localhost:3000');
+
+socket.on('connect', () => {
+    // Az eseményekhez tartozó kódok ide kerülnek
+    socket.emit('desk2', 'desk2 connected');
+    socket.on('History', (data) => {
+        console.log(data);
+        document.getElementById('history').innerHTML = '';
+        for(let i = 0; i < data.length; i++) {
+            document.getElementById('history').innerHTML += `<li> ${data[i]} <button class="del_btn" onclick="del(${i})">✅</button></li>`;
+        }
+    });
+});
+
+socket.on('disconnect', () => {
+    console.log('Kapcsolat megszakadt');
+});
+
+
+let del = (i) => {
+    socket.emit('del2', i);
+}
+
+let needHelp = () => {
+    socket.emit('desk2Need', 'desk2 need help');
+}
+
+let needTanu = () => {
+    socket.emit('desk2Need', 'desk2 need 18+');
+}
---
+++ public/desk3.js
@@ -0,0 +1,30 @@
+const socket = io('http://localhost:3000');
+
+socket.on('connect', () => {
+    // Az eseményekhez tartozó kódok ide kerülnek
+    socket.emit('desk3', 'desk3 connected');
+    socket.on('History', (data) => {
+        console.log(data);
+        document.getElementById('history').innerHTML = '';
+        for(let i = 0; i < data.length; i++) {
+            document.getElementById('history').innerHTML += `<li> ${data[i]} <button class="del_btn" onclick="del(${i})">✅</button></li>`;
+        }
+    });
+});
+
+socket.on('disconnect', () => {
+    console.log('Kapcsolat megszakadt');
+});
+
+
+let del = (i) => {
+    socket.emit('del3', i);
+}
+
+let needHelp = () => {
+    socket.emit('desk3Need', 'desk3 need help');
+}
+
+let needTanu = () => {
+    socket.emit('desk3Need', 'desk3 need 18+');
+}
